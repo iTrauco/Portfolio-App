63dc860c8c5c246c0a1edde3d524dcc67ef4a58c
Gage Henderson <gage@quangoinc.com>
2018-07-24 09:17:59 -0700

    Added the homeheader, now displays only when the typing intro isnt displayed

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import TypeEffect from '../../components/TypeEffect';
+import '../../styles/pages/home/home.css';
+import HomeHeader from './HomeHeader';
+
+class Home extends Component {
+  constructor(options) {
+    super(options);
+    this.state = {
+      introComplete: false
+    };
+  }
+
+  // The markup
+  render() {
+    var typingIntro;
+    var showPage;
+    if (
+      localStorage.getItem('hasVisited') == 'true' ||
+      this.state.introComplete
+    ) {
+      // Don't display typing intro
+      typingIntro = <div />;
+
+      // Display the page
+      showPage = true;
+    } else {
+      // Display the typing intro
+      typingIntro = (
+        <TypeEffect complete={this.completeTypeEffect.bind(this)} />
+      );
+
+      // Don't display the page
+      showPage = false;
+    }
+
+    return (
+      <div className="Home">
+        {typingIntro}
+        {this.state && <HomeHeader />}
+      </div>
+    );
+  }
+
+  // Trigger the completion of the intro typing effect
+  completeTypeEffect() {
+    // Change the state.
+    this.setState({ introComplete: true });
+
+    // Remember this user has visited before
+    localStorage.setItem('hasVisited', 'true');
+  }
+}
+
+export default Home;
---
+++ src/pages/Home/HomeHeader.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../../styles/pages/home/home-header.css';
+
+class HomeHeader extends Component {
+  render() {
+    return (
+      <div className="HomeHeader">
+        <div className="title">Gage Henderson</div>
+        <div className="title transition">Gage Henderson</div>
+      </div>
+    );
+  }
+}
+
+export default HomeHeader;
