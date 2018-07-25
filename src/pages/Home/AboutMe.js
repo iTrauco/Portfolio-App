7486635d7ada4463814405793000c6fb5a55d7be
Gage Henderson <gage@quangoinc.com>
2018-07-25 14:10:34 -0700

    Changed the layout of, and expanded upon the AboutMe section

--- src/pages/Home/AboutMe.js
+++ src/pages/Home/AboutMe.js
@@ -24,22 +24,29 @@ export default class AboutMe extends Component {
   render() {
     return (
       <div className="AboutMe">
-        <div className="side left">
+        <div className="side top">
           <div
             className="self-portrait"
             style={{ backgroundImage: `url("${photo}")` }}
           />
         </div>
-        <div className="side right">
+        <div className="side bottom">
           <div className="header-text">Who am I?</div>
           <div className="sub-text">
             My love for coding began at 11 when I got my first hand-me-down
             macbook. I started out making games in my free time, which
-            eventually led to me getting a full-time job as a web developer.<br />
+            eventually led to me getting a full-time job as a web developer.
             <br />
-            Since then, I have become familiar with an array of techonology:
-            HTML, CSS, SASS, Javascript, Jquery, ReactJS, NodeJS, ThreeJS, SQL,
-            and more.
+            <br />
+            When I'm not developing my coding skills, you can find me producing
+            music in the studio (my room,) and laying down vocals in the booth
+            (my closet.) I picked up music composition when I got that same
+            laptop, and it has helped me challenge my creative thinking every
+            day since.
+            <br />
+            <br />
+            I am primarily skilled at front-end development, being proficient in
+            HTML, CSS, SASS, Javascript, JQuery, ReactJS, and ThreeJS.
           </div>
         </div>
       </div>
