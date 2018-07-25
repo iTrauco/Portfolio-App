f09db4c4858f5b2aa2705bc7839a18b9bb2c20a0
Gage Henderson <gage@quangoinc.com>
2018-07-25 14:47:27 -0700

    Removed MyWork and AboutMe, too basic!!

--- src/pages/Home/HomeBody.js
+++ src/pages/Home/HomeBody.js
@@ -1,18 +1,11 @@
 import React, { Component } from 'react';
-import AboutMe from './AboutMe';
-import MyWork from './MyWork';
 
 class HomeBody extends Component {
   constructor(options) {
     super(options);
   }
   render() {
-    return (
-      <div className="HomeBody">
-        <AboutMe scrollY={this.props.scrollY} />
-        <MyWork scrollY={this.props.scrollY} />
-      </div>
-    );
+    return <div className="HomeBody" />;
   }
 }
 
