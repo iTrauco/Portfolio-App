77162e6dc86f6549b8c51772b3d51e4b0a2fedb6
Gage Henderson <gage@quangoinc.com>
2018-08-17 11:46:47 -0700

    Added a My-Services section component + stylesheet

--- src/pages/Home/HomeBody.js
+++ src/pages/Home/HomeBody.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import MyWork from './MyWork';
 import MyTech from './MyTech';
+import MyServices from './MyServices';
 
 class HomeBody extends Component {
   constructor(options) {
@@ -9,6 +10,7 @@ class HomeBody extends Component {
   render() {
     return (
       <div className="HomeBody">
+        <MyServices scrollY={this.props.scrollY} />
         <MyWork scrollY={this.props.scrollY} />
         <MyTech scrollY={this.props.scrollY} />
       </div>
---
+++ src/pages/Home/MyServices.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import '../../styles/pages/home/my-services.css';
+
+class MyServices extends Component {
+  render() {
+    return <div className="MyServices">A list of my services.</div>;
+  }
+}
+
+export default MyServices;
