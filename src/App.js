e72789f8d8a6763b83523c464bb5129ead40551c
Gage Henderson <gage@quangoinc.com>
2018-09-04 16:41:39 -0700

    Setup font-awesome

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Home from './pages/Home/Home';
 import './styles/global.css';
 import './styles/fonts.css';
+import './styles/font-awesome/css/all.css';
 
 class App extends Component {
   render() {
