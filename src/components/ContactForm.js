91c980583c1d4e0a29b953ca9d8c956c1abb8f3f
Gage Henderson <gage@quangoinc.com>
2018-09-04 16:40:08 -0700

    Created a contact form component and stylesheet

---
+++ src/components/ContactForm.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import '../styles/components/contact-form.css';
+
+class ContactForm extends Component {
+  render() {
+    return <div className="ContactForm">Contact Me:</div>;
+  }
+}
+
+export default ContactForm;
