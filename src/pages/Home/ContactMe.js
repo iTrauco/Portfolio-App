d283de61b8fa55428b608e42a463f304e65c7b2a
Gage Henderson <gage@quangoinc.com>
2018-09-05 10:17:34 -0700

    Got the basics of the contact blade complete

--- src/pages/Home/ContactMe.js
+++ src/pages/Home/ContactMe.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 import '../../styles/pages/home/contact-me.css';
 
 class ContactMe extends Component {
   render() {
     return (
       <div className="ContactMe">
-        <div className="content" />
+        <div className="content">
+          {/* The Copy */}
+          <div className="title">Contact Me</div>
+          <div className="line" />
+          <div className="sub-text">
+            You can contact me via email, or the form linked below.
+          </div>
+
+          {/* CTA's */}
+          <div className="contact-container">
+            <div className="button-container">
+              <div className="button">
+                <i className="fas fa-pencil-alt" />
+                Contact Me
+              </div>
+            </div>
+            <div className="bar" />
+            <div className="email" onClick={this.copyEmail.bind(this)}>
+              gageprod@gmail.com
+            </div>
+          </div>
+        </div>
       </div>
     );
+  }
+
+  // Copy the email address to clipboard on click
+  copyEmail() {
+    $('.email').select();
+    document.execCommand('copy');
   }
 }
 
