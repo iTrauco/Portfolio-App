5a471527787223dfdda189d0ee386c27c2e03ed6
Gage Henderson <gage@quangoinc.com>
2018-09-05 14:31:43 -0700

    The header canvas now only updates if it is within the users viewport

--- src/components/CanvasBall/CanvasBall.js
+++ src/components/CanvasBall/CanvasBall.js
@@ -32,50 +32,52 @@ export default class CanvasBall {
   }
 
   update() {
-    for (var i = 0; i < this.balls.length; i++) {
-      var ball = this.balls[i];
-      this.c.fillStyle = '#596DFF';
-      this.c.beginPath();
-      this.c.arc(ball.x, ball.y, ball.size, 0, 2 * Math.PI, false);
-      this.c.fill();
+    if (this.isOnScreen()) {
+      for (var i = 0; i < this.balls.length; i++) {
+        var ball = this.balls[i];
+        this.c.fillStyle = '#596DFF';
+        this.c.beginPath();
+        this.c.arc(ball.x, ball.y, ball.size, 0, 2 * Math.PI, false);
+        this.c.fill();
 
-      ball.x += ball.xvel;
-      ball.y += ball.yvel;
-      if (ball.x + ball.size / 2 > this.c.canvas.width) {
-        ball.x = this.c.canvas.width - ball.size / 2;
-        ball.xvel *= -bounce;
-      } else if (ball.x - ball.size / 2 < 0) {
-        ball.x = ball.size / 2;
-        ball.xvel *= -bounce;
+        ball.x += ball.xvel;
+        ball.y += ball.yvel;
+        if (ball.x + ball.size / 2 > this.c.canvas.width) {
+          ball.x = this.c.canvas.width - ball.size / 2;
+          ball.xvel *= -bounce;
+        } else if (ball.x - ball.size / 2 < 0) {
+          ball.x = ball.size / 2;
+          ball.xvel *= -bounce;
+        }
+        if (ball.y - ball.size / 2 < 0) {
+          ball.y = ball.size / 2;
+          ball.yvel *= -bounce;
+        } else if (ball.y + ball.size / 2 > this.c.canvas.height) {
+          ball.y = this.c.canvas.height - ball.size / 2;
+          ball.yvel *= -bounce;
+        }
+
+        var angle = Math.atan2(this.mouse.y - ball.y, this.mouse.x - ball.x);
+        var distance =
+          GageLib.math.getDistance(ball.x, ball.y, this.mouse.x, this.mouse.y) /
+          2;
+        var force = mouseForce / (distance * 0.1);
+
+        // Apply force from mouse if it is large enough
+        if (force >= minForce) {
+          ball.xvel += Math.cos(angle + Math.PI) * force;
+          ball.yvel += Math.sin(angle + Math.PI) * force;
+          ball.dir = angle + Math.PI;
+        } else if (ball.dir) {
+          ball.dir += ball.moveDir;
+          ball.xvel += Math.cos(ball.dir) * force;
+          ball.yvel += Math.sin(ball.dir) * force;
+        }
+
+        // Apply Friction
+        ball.xvel *= friction;
+        ball.yvel *= friction;
       }
-      if (ball.y - ball.size / 2 < 0) {
-        ball.y = ball.size / 2;
-        ball.yvel *= -bounce;
-      } else if (ball.y + ball.size / 2 > this.c.canvas.height) {
-        ball.y = this.c.canvas.height - ball.size / 2;
-        ball.yvel *= -bounce;
-      }
-
-      var angle = Math.atan2(this.mouse.y - ball.y, this.mouse.x - ball.x);
-      var distance =
-        GageLib.math.getDistance(ball.x, ball.y, this.mouse.x, this.mouse.y) /
-        2;
-      var force = mouseForce / (distance * 0.1);
-
-      // Apply force from mouse if it is large enough
-      if (force >= minForce) {
-        ball.xvel += Math.cos(angle + Math.PI) * force;
-        ball.yvel += Math.sin(angle + Math.PI) * force;
-        ball.dir = angle + Math.PI;
-      } else if (ball.dir) {
-        ball.dir += ball.moveDir;
-        ball.xvel += Math.cos(ball.dir) * force;
-        ball.yvel += Math.sin(ball.dir) * force;
-      }
-
-      // Apply Friction
-      ball.xvel *= friction;
-      ball.yvel *= friction;
     }
     window.requestAnimationFrame(this.update.bind(this));
   }
@@ -105,5 +107,14 @@ export default class CanvasBall {
   trackMouse(e) {
     this.mouse.x = e.pageX;
     this.mouse.y = e.pageY;
+  }
+
+  // Check if the canvas is even on the screen
+  isOnScreen() {
+    if ($(window).scrollTop() >= this.c.canvas.height) {
+      return false;
+    } else {
+      return true;
+    }
   }
 }
