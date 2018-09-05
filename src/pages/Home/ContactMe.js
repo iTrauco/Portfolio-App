a84b6c76086212318cefbc81765cc14bf226fa00
Gage Henderson <gage@quangoinc.com>
2018-09-05 12:55:30 -0700

    Added some on scroll animation to the contactme blade

--- src/pages/Home/ContactMe.js
+++ src/pages/Home/ContactMe.js
@@ -3,20 +3,44 @@ import $ from 'jquery';
 import '../../styles/pages/home/contact-me.css';
 
 class ContactMe extends Component {
+  componentDidMount() {
+    // Anim on scroll
+    var els = $('.ContactMe .anim-on-scroll');
+    this.scrollableElements = [];
+    for (var i = 0; i < els.length; i++) {
+      var el = els.eq(i);
+      this.scrollableElements.push({
+        el: el,
+        y: el.offset().top + el.height()
+      });
+    }
+  }
+  componentWillReceiveProps(newProps) {
+    this.checkScroll(newProps.scrollY);
+  }
+  checkScroll(scrollY) {
+    for (var i = 0; i < this.scrollableElements.length; i++) {
+      var scrollable = this.scrollableElements[i];
+      if (scrollY + window.innerHeight >= scrollable.y) {
+        scrollable.el.addClass('anim');
+      }
+    }
+  }
+
   render() {
     return (
       <div className="ContactMe">
         <div className="content">
           {/* The Copy */}
-          <div className="title">Contact Me</div>
-          <div className="line" />
-          <div className="sub-text">
+          <div className="title anim-on-scroll">Contact Me</div>
+          <div className="line anim-on-scroll" />
+          <div className="sub-text anim-on-scroll">
             Want to hire me? Need some contract work? Just want to chat? You can
             contact me via email, or the form linked below.
           </div>
 
           {/* CTA's */}
-          <div className="contact-container">
+          <div className="contact-container anim-on-scroll">
             <div className="button-container">
               <div className="button">
                 <i className="fas fa-pencil-alt" />
