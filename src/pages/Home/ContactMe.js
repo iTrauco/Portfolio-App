cf059b58745386b334bd7f7401c0017cd40ea956
Gage Henderson <gage@quangoinc.com>
2018-09-05 09:44:25 -0700

    Created a ContactMe component and stylesheet for the contactme blade

---
+++ src/pages/Home/ContactMe.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../../styles/pages/home/contact-me.css';
+
+class ContactMe extends Component {
+  render() {
+    return (
+      <div className="ContactMe">
+        <div className="content" />
+      </div>
+    );
+  }
+}
+
+export default ContactMe;
