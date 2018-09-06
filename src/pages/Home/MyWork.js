add6ff4034bcb365d93c9823cb8cd19452843ef5
Gage Henderson <gage@quangoinc.com>
2018-09-06 16:05:17 -0700

    Starting on the MyWork section, created some copy and content for a first project, but am not rendering it yet

--- src/pages/Home/MyWork.js
+++ src/pages/Home/MyWork.js
@@ -1,14 +1,47 @@
 import React, { Component } from 'react';
 import '../../styles/pages/home/my-work.css';
 
+// The MyWork Blade Component
 class MyWork extends Component {
   render() {
     return (
       <div className="MyWork">
-        <div className="title">My Work:</div>
+        <div className="content">
+          <div className="title">My Work:</div>
+        </div>
       </div>
     );
   }
 }
+
+/**
+ * All of the projects listed in the MyWork blade.
+ *
+ * Quick notes:
+ *  - In the images array, the filepaths should be
+ *    relative to the `src/assets` directory.
+ *
+ */
+var projects = [
+  {
+    name: 'Canby Ed Foundation',
+    description: `
+      The Canby Educational Foundation is an organization that supplies
+      grants to local schools. Their previous website was built with React,
+      using a custom-made CMS, everything worked fine but it wasn't quite
+      as flexible as they wanted - So I was tasked with recreating the site
+      from scratch in wordpress.
+
+      This project was a lot of fun. I used the
+      <a href="https://www.advancedcustomfields.com/" target="_blank">
+      Advanced Custom Fields</a> which allowed me to easily make nearly 
+      any element on the site customizable for the client. There is also a 
+      <a href="https://canbyedfoundation.org/grant-form/">grant application
+      form</a> on the site. Which, in making it, helped me learn a lot about
+      Wordpress & smtp.
+    `,
+    images: ['/canby-ed-foundation/1.png', '/canby-ed-foundation/2.png']
+  }
+];
 
 export default MyWork;
