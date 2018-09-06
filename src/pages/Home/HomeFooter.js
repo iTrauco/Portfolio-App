aa2878cc5de0f72c6df5530d7e43171c51c13f69
Gage Henderson <gage@quangoinc.com>
2018-09-06 13:02:21 -0700

    Added a HomeFooter component and stylesheet

---
+++ src/pages/Home/HomeFooter.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import '../../styles/pages/home/home-footer.css';
+
+class HomeFooter extends Component {
+  render() {
+    return <div className="HomeFooter" />;
+  }
+}
+
+export default HomeFooter;
