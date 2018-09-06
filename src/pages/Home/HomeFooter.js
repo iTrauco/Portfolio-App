c8421f9d810ad73b96feb6cd4c31ee3a8ebb7fea
Gage Henderson <gage@quangoinc.com>
2018-09-06 15:16:55 -0700

    Completed the content and styling of the footer

--- src/pages/Home/HomeFooter.js
+++ src/pages/Home/HomeFooter.js
@@ -3,7 +3,45 @@ import '../../styles/pages/home/home-footer.css';
 
 class HomeFooter extends Component {
   render() {
-    return <div className="HomeFooter" />;
+    return (
+      <div className="HomeFooter">
+        <div className="content">
+          <div className="text-container">
+            <div>Website Created by Gage Henderson</div>
+            <div>gageprod@gmail.com</div>
+          </div>
+          <div className="line" />
+          <div className="link-container">
+            {/* Github */}
+            <a
+              className="link"
+              href="https://github.com/quangogage"
+              target="_blank"
+            >
+              <i className="fab fa-github" />
+            </a>
+
+            {/* Stack Overflow */}
+            <a
+              className="link"
+              href="https://stackoverflow.com/users/8322279/gage-hendy-ya-boy?tab=profile"
+              target="_blank"
+            >
+              <i className="fab fa-stack-overflow" />
+            </a>
+
+            {/* Codepen */}
+            <a
+              className="link"
+              href="https://codepen.io/quangogage/"
+              target="_blank"
+            >
+              <i className="fab fa-codepen" />
+            </a>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
