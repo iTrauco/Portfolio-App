5c7b56077f281149766ed5433aac856aad5619c4
Gage Henderson <gage@quangoinc.com>
2018-09-06 15:38:53 -0700

    Created a MyWork component and stylesheet. This will actually be a section of the page that displays my work

--- src/pages/Home/HomeBody.js
+++ src/pages/Home/HomeBody.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import MyDesign from './MyDesign';
 import MyTech from './MyTech';
 import MyServices from './MyServices';
+import MyWork from './MyWork';
 import ContactMe from './ContactMe';
 
 class HomeBody extends Component {
@@ -11,6 +12,7 @@ class HomeBody extends Component {
         {/*<MyServices scrollY={this.props.scrollY} />*/}
         <MyDesign scrollY={this.props.scrollY} />
         <MyTech scrollY={this.props.scrollY} />
+        <MyWork scrollY={this.props.scrollY} />
         <ContactMe
           scrollY={this.props.scrollY}
           triggerMessage={this.props.triggerMessage}
---
+++ src/pages/Home/MyWork.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../../styles/pages/home/my-work.css';
+
+class MyWork extends Component {
+  render() {
+    return (
+      <div className="MyWork">
+        <div className="title">My Work:</div>
+      </div>
+    );
+  }
+}
+
+export default MyWork;
